f40573a93a338c872a03bfe8224ebe86bca6205c
Ewelina Boczar <ewelina.boczar@student.pk.edu.pl>
2020-11-18 21:13:17 +0100

    uzupenienia

---
+++ matrix.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "matrix.h"
+
+using namespace std;
+
+matrix::matrix (int r, int c)
+{
+    
+}
---
+++ matrix.h
@@ -0,0 +1,11 @@
+#include <iostream>
+
+class matrix
+{
+    int row, column;
+    int square;
+    
+    public:
+    matrix(int,int);
+
+};
